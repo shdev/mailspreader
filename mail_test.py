30d6b014803dd1b379d27266ed376655df15a20a
Sebastian Holtz <mail@sh-dev.de>
2014-12-17 23:58:54 +0100

    During Refactor

--- mail_test.py
+++ mail_test.py
@@ -13,24 +13,69 @@ class MailSpreader(object):
     """The class is basic script for sending a mails to \
     multiply destinantions"""
 
+    CONFIG_SECTION_INPUT_SERVER = "INPUT-SERVER"
+    CONFIG_SECTION_MAIN = "MAIN"
+    CONFIG_SECTION_SEND_SERVER = "SEND-SERVER"
+    CONFIG_SECTION_STORE_SERVER = "STORE-SERVER"
+
+    CONFIG_DEFAULT_DELETE_AFTER_PROCESSING = False
+    CONFIG_DEFAULT_MARK_AS_SEEN_AFTER_PROCESSING = True
+    CONFIG_DEFAULT_SEARCH_FILTER = 'NEW'
+
+    CONFIG_FIELD_DELETE_AFTER_PROCESSING = 'delete_after_processing'
+    CONFIG_FIELD_HOST = 'host'
+    CONFIG_FIELD_LOGFIlE = 'logfile'
+    CONFIG_FIELD_MAILBOX = 'mailbox'
+    CONFIG_FIELD_MARK_AS_SEEN_AFTER_PROCESSING = \
+        'mark_as_seen_after_processing'
+    CONFIG_FIELD_PASSWORD = 'password'
+    CONFIG_FIELD_PORT = 'port'
+    CONFIG_FIELD_SEARCH_FILTER = 'search_filter'
+    CONFIG_FIELD_USE_SSL = 'use_ssl'
+    CONFIG_FIELD_USERNAME = 'username'
+
+    HEADER_EXCLUDES = {'subject', 'from', 'date', 'content-type',
+                       'mime-version', 'content-transfer-encoding',
+                       'message-id', 'x-mailer'}
+
     def __init__(self):
         super(MailSpreader, self).__init__()
 
     @classmethod
     def login_imap(cls, config, section):
-        if config.getboolean(section, 'use_ssl'):
+        if config.getboolean(section, MailSpreader.CONFIG_FIELD_USE_SSL):
             imap_server = imaplib.IMAP4_SSL(
-                host=config.get(section, 'host'),
-                port=config.get(section, 'port'))
+                host=config.get(section, MailSpreader.CONFIG_FIELD_HOST),
+                port=config.get(section, MailSpreader.CONFIG_FIELD_PORT))
         else:
             imap_server = imaplib.IMAP4(
-                host=config.get(section, 'host'),
-                port=config.get(section, 'port'))
+                host=config.get(section, MailSpreader.CONFIG_FIELD_HOST),
+                port=config.get(section, MailSpreader.CONFIG_FIELD_PORT))
 
-        imap_server.login(config.get(section, 'username'),
-                          config.get(section, 'password'))
+        imap_server.login(config.get(section,
+                                     MailSpreader.CONFIG_FIELD_USERNAME),
+                          config.get(section,
+                                     MailSpreader.CONFIG_FIELD_PASSWORD))
 
         return imap_server
+
+    @classmethod
+    def login_smtp(cls, config, section):
+        if config.getboolean(section, MailSpreader.CONFIG_FIELD_USE_SSL):
+            smtp_server = smtplib.SMTP_SSL(
+                host=config.get(section, MailSpreader.CONFIG_FIELD_HOST),
+                port=config.get(section, MailSpreader.CONFIG_FIELD_PORT))
+        else:
+            smtp_server = smtplib.SMTP(
+                host=config.get(section, MailSpreader.CONFIG_FIELD_HOST),
+                port=config.get(section, MailSpreader.CONFIG_FIELD_PORT))
+
+        smtp_server.login(config.get(section,
+                                     MailSpreader.CONFIG_FIELD_USERNAME),
+                          config.get(section,
+                                     MailSpreader.CONFIG_FIELD_PASSWORD))
+
+        return smtp_server
 
     def main(self):
         self.preparations()
@@ -55,127 +100,122 @@ class MailSpreader(object):
         self.args = parser.parse_args()
 
     def process_config(self):
-        self.cfg = configparser.ConfigParser()
+        self.cfg = configparser.ConfigParser(allow_no_value=True)
         self.cfg.read(self.args.configfile)
-        print(self.cfg.sections())
-        for section in self.cfg.sections():
-            print('%s\n---------' % section)
-            for option in self.cfg.options(section):
-                print("'%s':'%s'" % (option, self.cfg.get(section, option)))
+        # print(self.cfg.sections())
+        # for section in self.cfg.sections():
+        #     print('%s\n---------' % section)
+        #     for option in self.cfg.options(section):
+        #         print("'%s':'%s'" % (option, self.cfg.get(section, option)))
 
     def setup_logging(self):
         loglevel = self.args.loglevel
         numeric_level = getattr(logging, loglevel.upper(), None)
         if not isinstance(numeric_level, int):
             raise ValueError('Invalid log level: %s' % loglevel)
-        logging.basicConfig(filename=self.cfg.get('MAIN', 'logfile'),
+        logging.basicConfig(filename=
+                            self.cfg.get(MailSpreader.CONFIG_SECTION_MAIN,
+                                         MailSpreader.CONFIG_FIELD_LOGFIlE),
                             level=numeric_level,
                             format='%(asctime)s [%(levelname)s]'
                             ': %(message)s')
-
-    def login_input_server(self):
-        if self.cfg.getboolean('INPUT-SERVER', 'use_ssl'):
-            self.input_server = imaplib.IMAP4_SSL(
-                host=self.cfg.get('INPUT-SERVER', 'host'),
-                port=self.cfg.get('INPUT-SERVER', 'port'))
-        else:
-            self.input_server = imaplib.IMAP4(
-                host=self.cfg.get('INPUT-SERVER', 'host'),
-                port=self.cfg.get('INPUT-SERVER', 'port'))
-
-        self.input_server.login(self.cfg.get('INPUT-SERVER', 'username'),
-                                self.cfg.get('INPUT-SERVER', 'password'))
-
-    def login_store_server(self):
-        if self.cfg.getboolean('INPUT-SERVER', 'use_ssl'):
-            self.input_server = imaplib.IMAP4_SSL(
-                host=self.cfg.get('INPUT-SERVER', 'host'),
-                port=self.cfg.get('INPUT-SERVER', 'port'))
-        else:
-            self.input_server = imaplib.IMAP4(
-                host=self.cfg.get('INPUT-SERVER', 'host'),
-                port=self.cfg.get('INPUT-SERVER', 'port'))
-
-        self.input_server.login(self.cfg.get('INPUT-SERVER', 'username'),
-                                self.cfg.get('INPUT-SERVER', 'password'))
 
     def preparations(self):
         self.parse_arguments()
         self.process_config()
         self.setup_logging()
-        logging.debug("Preparation finished")
+        logging.info("Mailspreader starts")
 
-        self.input_server = MailSpreader.login_imap(self.cfg, 'INPUT-SERVER')
+        self.input_server = MailSpreader.login_imap(
+            self.cfg, MailSpreader.CONFIG_SECTION_INPUT_SERVER)
         logging.debug("logged-in: input server")
 
-        self.store_server = MailSpreader.login_imap(self.cfg, 'STORE-SERVER')
+        input_mailbox = self.cfg.get(MailSpreader.CONFIG_SECTION_INPUT_SERVER,
+                                     MailSpreader.CONFIG_FIELD_MAILBOX,
+                                     fallback=None)
+        if input_mailbox is None:
+            print(self.input_server.select())
+            logging.debug("selected default mailbox")
+        else:
+            self.input_server.select(input_mailbox)
+            logging.debug(
+                "selected mailbox: %s" % input_mailbox)
+
+        self.store_server = MailSpreader.login_imap(
+            self.cfg, MailSpreader.CONFIG_SECTION_STORE_SERVER)
         logging.debug("logged-in: store server")
+
+        self.send_server = MailSpreader.login_smtp(
+            self.cfg, MailSpreader.CONFIG_SECTION_SEND_SERVER)
+        logging.debug("logged-in: send server")
+        logging.debug("Preparation finished")
 
     def process_data(self):
 
-        O = smtplib.SMTP('wp292.webpack.hosteurope.de')
-        O.login("wp1134078-shtest2", "posttest2")
-
-        liste = self.store_server.list()
-
-        print(liste[1])
-
-        newliste = []
-        for x in liste[1]:
-            newliste.append(x.decode())
-
-        print(newliste)
-
         try:
-            self.input_server.select()
-            print(self.store_server.select('"Sent\ Messages"'))
 
-            print('I am here')
+            search_filter = self.cfg.get(
+                MailSpreader.CONFIG_SECTION_INPUT_SERVER,
+                MailSpreader.CONFIG_FIELD_SEARCH_FILTER,
+                fallback=MailSpreader.CONFIG_DEFAULT_SEARCH_FILTER)
 
-            typ, data = self.input_server.search(None, 'ALL')
+            mark_as_seen = self.cfg.getboolean(
+                MailSpreader.CONFIG_SECTION_INPUT_SERVER,
+                MailSpreader.CONFIG_FIELD_MARK_AS_SEEN_AFTER_PROCESSING,
+                fallback=MailSpreader.CONFIG_DEFAULT_MARK_AS_SEEN_AFTER_PROCESSING)
+
+            delete_mail = self.cfg.getboolean(
+                MailSpreader.CONFIG_SECTION_INPUT_SERVER,
+                MailSpreader.CONFIG_FIELD_DELETE_AFTER_PROCESSING,
+                fallback=MailSpreader.CONFIG_DEFAULT_DELETE_AFTER_PROCESSING)
+
+            logging.debug("Used search filter: %s" % search_filter)
+
+            typ, data = self.input_server.search(None, search_filter)
+
             for num in data[0].split():
                 typ, data = self.input_server.fetch(num, '(RFC822)')
-                # print("NUM:", num)
-                # print("DATA:", data)
-                # print("type:", typ)
-                # print('Message %s\n%s\n' % (num, data[0][1]))
-                # N.append(None, None, None, data[0][1])
-                msg1 = email.message_from_string(data[0][1].decode('utf-8'))
+                msg1 = email.message_from_string(data[0][1].decode())
 
                 print(msg1.keys())
 
                 for key in msg1.keys():
                     print(key, key.lower())
-                    if key.lower() not in {'subject', 'from', 'date',
-                                           'content-type', 'mime-version',
-                                           'content-transfer-encoding',
-                                           'message-id', 'x-mailer'}:
+                    if key.lower() not in MailSpreader.HEADER_EXCLUDES:
                         del msg1[key]
                 msg1['To'] = 'mail@sh-dev.de'
 
                 msg1['X-SHdever'] = 'mail@sh-dev.de'
                 print(msg1.keys())
 
-                O.sendmail("mail@sh-dev.de", "mail@sh-dev.de", str(msg1))
+                self.send_server.sendmail("mail@sh-dev.de",
+                                          "mail@sh-dev.de", str(msg1))
 
                 self.store_server.append('"Sent\ Messages"',
                                          None, None,
                                          str(msg1).encode())
-                self.input_server.store(num, '+FLAGS', '\\Deleted')
+
+                if mark_as_seen:
+                    self.input_server.store(num, '+FLAGS', '\\SEEN')
+
+                if delete_mail:
+                    self.input_server.store(num, '+FLAGS', '\\Deleted')
 
                 print('--------------------')
-            self.input_server.expunge()
+
+            if delete_mail:
+                self.input_server.expunge()
         except:
             sys.stderr.write("Something went here extremly wrong, and I don't "
                              "know why\n")
             self.logging.critical("Something went here extremly wrong, "
                                   "and I don't know why")
         finally:
+
             self.input_server.close()
             self.input_server.logout()
-            self.store_server.close()
             self.store_server.logout()
-            O.close()
+            self.send_server.close()
 
 if __name__ == '__main__':
     spreader = MailSpreader()
